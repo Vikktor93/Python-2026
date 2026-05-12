d0d31ec10ed53c97599c131843d4114a81f7ebb5
Victor Saldivia <viktorsaldivia93@gmail.com>
2026-05-12 17:00:55 -0400

    Subiendo archivo de Listas

---
+++ 03-listas.py
@@ -0,0 +1,12 @@
+# LISTAS
+
+# Primera Forma de Declaración de Listas (Lista Mixta)
+lista1 = ['Victor', 32, True, "Victor", "Victor", "Victor"]
+
+# Segunda Forma de Declaración de Listas (Lista Númerica)
+n = list([1,2,3,4,5])
+
+# Métodos para las Listas
+# 01- COUNT() - Contar la cantidad de concurrencias de un elemento
+print(lista1.count('Victor'))
+
